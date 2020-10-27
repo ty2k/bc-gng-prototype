53b5548229c4055c5741ae696c86b46542cba00a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-10-27 10:50:10 -0700

    Fix styled-component dynamically created component error in Header

--- react-app/src/stories/Header.js
+++ react-app/src/stories/Header.js
@@ -9,73 +9,73 @@ import UserPanel from "./UserPanel";
 import { ReactComponent as Logo } from "./assets/BCID_V_rgb_pos.svg";
 import { ReactComponent as HamburgerIcon } from "./assets/bars-solid.svg";
 
+const HeaderStyled = styled.header`
+  div.wrapper {
+    align-items: center;
+    border-bottom: 2px solid #d1d1d1;
+    display: flex;
+    font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
+    height: 80px;
+    justify-content: space-between;
+    padding: 0 10px;
+  }
+
+  div.wrapper > div {
+    display: flex;
+    align-items: center;
+  }
+
+  div.wrapper > div > svg#logo {
+    display: inline-block;
+    height: 80px;
+    width: 84px;
+    vertical-align: top;
+  }
+
+  div.wrapper > div > span.span--title {
+    font-weight: 900;
+    font-size: 21px;
+    line-height: 1;
+    margin: 6px 0 6px 10px;
+    display: inline-block;
+  }
+
+  div.wrapper > div > button {
+    margin-left: 11px;
+  }
+  div.wrapper > div > button:first-of-type {
+    margin-left: 32px;
+  }
+  div.wrapper > div > button:last-of-type {
+    margin-right: 11px;
+  }
+
+  div.wrapper > div > a#menu-icon {
+    text-decoration: none;
+  }
+  div.wrapper > div > a#menu-icon span {
+    color: #313132;
+    display: block;
+    font-size: 15px;
+    text-align: center;
+  }
+  div.wrapper > div > a#menu-icon svg {
+    box-sizing: border-box;
+    color: #313132;
+    display: block;
+    height: 44px;
+    margin: 0 10px;
+    padding: 0 10px;
+    width: 44px;
+  }
+`;
+
 function Header({ title, userSession }) {
   const [navHidden, setNavHidden] = useState(true);
 
   function toggleMenu(event) {
     setNavHidden(!navHidden);
   }
-
-  const HeaderStyled = styled.header`
-    div.wrapper {
-      align-items: center;
-      border-bottom: 2px solid #d1d1d1;
-      display: flex;
-      font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
-      height: 80px;
-      justify-content: space-between;
-      padding: 0 10px;
-    }
-
-    div.wrapper > div {
-      display: flex;
-      align-items: center;
-    }
-
-    div.wrapper > div > svg#logo {
-      display: inline-block;
-      height: 80px;
-      width: 84px;
-      vertical-align: top;
-    }
-
-    div.wrapper > div > span.span--title {
-      font-weight: 900;
-      font-size: 21px;
-      line-height: 1;
-      margin: 6px 0 6px 10px;
-      display: inline-block;
-    }
-
-    div.wrapper > div > button {
-      margin-left: 11px;
-    }
-    div.wrapper > div > button:first-of-type {
-      margin-left: 32px;
-    }
-    div.wrapper > div > button:last-of-type {
-      margin-right: 11px;
-    }
-
-    div.wrapper > div > a#menu-icon {
-      text-decoration: none;
-    }
-    div.wrapper > div > a#menu-icon span {
-      color: #313132;
-      display: block;
-      font-size: 15px;
-      text-align: center;
-    }
-    div.wrapper > div > a#menu-icon svg {
-      box-sizing: border-box;
-      color: #313132;
-      display: block;
-      height: 44px;
-      margin: 0 10px;
-      padding: 0 10px;
-      width: 44px;
-    }
-  `;
 
   return (
     <HeaderStyled>
