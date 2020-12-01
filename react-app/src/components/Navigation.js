b45c177d965cf3ebc1a4b3e3e7d31b30a49dcb74
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-12-01 09:59:19 -0800

    Refactor Navigation component to allow entire cards to act as links

--- react-app/src/components/Navigation.js
+++ react-app/src/components/Navigation.js
@@ -29,7 +29,7 @@ const Section = styled.div`
   }
 `;
 
-const Card = styled.div`
+const StyledCard = styled.div`
   border: 1px solid #d1d1d1;
   padding: 16px;
 
@@ -69,6 +69,18 @@ const Card = styled.div`
   }
 `;
 
+const StyledCardAnchorLink = styled.a`
+  color: inherit;
+  display: flex;
+  text-decoration: inherit;
+`;
+
+const StyledCardRouterLink = styled(Link)`
+  color: inherit;
+  display: flex;
+  text-decoration: inherit;
+`;
+
 function Icon({ id }) {
   const Icon = imageService.getSvg(id);
 
@@ -76,6 +88,70 @@ function Icon({ id }) {
     return <Icon />;
   } else {
     return null;
+  }
+}
+
+function CardAnchorLink({ href, children }) {
+  return <StyledCardAnchorLink href={href}>{children}</StyledCardAnchorLink>;
+}
+
+function CardRouterLink({ href, children }) {
+  return <StyledCardRouterLink to={href}>{children}</StyledCardRouterLink>;
+}
+function Card({ icon, title, description, links, cardLink }) {
+  if (cardLink && cardLink.external === true) {
+    return (
+      <CardAnchorLink href={cardLink.href}>
+        <StyledCard>
+          <div className="card--icon-title">
+            {icon && <Icon id={icon} />}
+            {title && <h3>{title}</h3>}
+            <Icon id="external-link-alt-solid.svg" />
+          </div>
+          {description && <p>{description}</p>}
+        </StyledCard>
+      </CardAnchorLink>
+    );
+  } else if (cardLink && cardLink.external === false) {
+    return (
+      <CardRouterLink href={cardLink.href}>
+        <StyledCard>
+          <div className="card--icon-title">
+            {icon && <Icon id={icon} />}
+            {title && <h3>{title}</h3>}
+          </div>
+          {description && <p>{description}</p>}
+        </StyledCard>
+      </CardRouterLink>
+    );
+  } else {
+    return (
+      <StyledCard>
+        <div className="card--icon-title">
+          {icon && <Icon id={icon} />}
+          {title && <h3>{title}</h3>}
+        </div>
+        {description && <p>{description}</p>}
+        {links && links.length > 0 && (
+          <ul>
+            {links.map((link, index) => {
+              return (
+                <li key={`li-${index}`}>
+                  {link.external ? (
+                    <>
+                      <a href={link}>{link.label}</a>
+                      <Icon id={"external-link-alt-solid.svg"} />
+                    </>
+                  ) : (
+                    <Link to={link.href}>{link.label}</Link>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </StyledCard>
+    );
   }
 }
 
@@ -96,35 +172,10 @@ function Navigation({ search, sections }) {
                   section.cards.length > 0 &&
                   section.cards.map((card, cardIndex) => {
                     return (
-                      <Card key={`section-${index}-card-${cardIndex}`}>
-                        <div className="card--icon-title">
-                          {card.icon && <Icon id={card.icon} />}
-                          {card.title && <h3>{card.title}</h3>}
-                        </div>
-                        {card.description && <p>{card.description}</p>}
-                        {card.links && card.links.length > 0 && (
-                          <ul>
-                            {card.links.map((link, linkIndex) => {
-                              return (
-                                <li
-                                  key={`section-${index}-card-${cardIndex}-li-${linkIndex}`}
-                                >
-                                  {link.external ? (
-                                    <>
-                                      <a href={link}>{link.label}</a>
-                                      <Icon
-                                        id={"external-link-alt-solid.svg"}
-                                      />
-                                    </>
-                                  ) : (
-                                    <Link to={link.href}>{link.label}</Link>
-                                  )}
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        )}
-                      </Card>
+                      <Card
+                        key={`section-${index}-card-${cardIndex}`}
+                        {...card}
+                      />
                     );
                   })}
               </Section>
@@ -134,6 +185,26 @@ function Navigation({ search, sections }) {
     </>
   );
 }
+
+Icon.propTypes = {
+  id: propTypes.string,
+};
+Card.propTypes = {
+  title: propTypes.string,
+  description: propTypes.string,
+  links: propTypes.arrayOf(
+    propTypes.shape({
+      href: propTypes.string,
+      label: propTypes.string,
+    })
+  ),
+  cardLink: propTypes.shape({
+    href: propTypes.string,
+    external: propTypes.bool,
+  }),
+};
+
+Card.defaultProps = {};
 
 Navigation.propTypes = {
   search: propTypes.shape({
