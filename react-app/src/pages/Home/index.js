332218fb0d6ca23152c1b4e3c704eb16bbce5644
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-12-14 13:46:15 -0800

    Removed unused propTypes definitions in top level content pages

--- react-app/src/pages/Home/index.js
+++ react-app/src/pages/Home/index.js
@@ -1,15 +1,7 @@
 import React from "react";
-import propTypes from "prop-types";
 
 function Home() {
   return <p>Province of British Columbia</p>;
 }
-
-Home.propTypes = {
-  title: propTypes.string,
-  breadcrumbs: propTypes.array,
-};
-
-Home.defaultProps = {};
 
 export default Home;
--- react-app/src/pages/JobsHR/index.js
+++ react-app/src/pages/JobsHR/index.js
@@ -1,15 +1,7 @@
 import React from "react";
-import propTypes from "prop-types";
 
 function JobsHR() {
   return <p>Jobs &amp; HR page</p>;
 }
-
-JobsHR.propTypes = {
-  title: propTypes.string,
-  breadcrumbs: propTypes.array,
-};
-
-JobsHR.defaultProps = {};
 
 export default JobsHR;
