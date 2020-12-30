2a99943c1d0cd146790587229fbc53464f9426ec
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-12-29 18:52:33 -0800

    Add Entertainment Industry ESB content page

---
+++ react-app/src/pages/Themes/Employment-Business-and-Economic-Development/Employment-Standards-and-Workplace-Safety/Employment-Standards/Hiring-Employees/Hiring-Young-People/Entertainment-Industry/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Content from "../../../../../../../../components/Content";
+import { content } from "./data";
+
+function EntertainmentIndustry() {
+  return <Content content={content} />;
+}
+
+export default EntertainmentIndustry;
