262b16552e4649da1e96836bc4f714756567452e
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-01-11 15:49:40 -0800

    Add RTB Dispute Resolution Applications page

---
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/data.js
@@ -0,0 +1,68 @@
+const sections = [
+  {
+    title: "",
+    cards: [
+      {
+        title: "Apply for a Dispute Resolution Application",
+        description: "Resolve disputes related to a tenancy.",
+      },
+      {
+        title: "Apply for a Tenant Direct Request",
+        description:
+          "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+      },
+      {
+        title: "Apply for a Landlord Direct Request",
+        description:
+          "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+      },
+      {
+        title: "Dispute Access Site",
+        description: "Are you a respondent? Add your evidence online!",
+      },
+      {
+        title: "View or Modify an Existing Application",
+        links: [
+          {
+            href: "",
+            label: "Amend or Update an Application",
+          },
+          {
+            href: "",
+            label: "Make a Cross Application",
+          },
+          {
+            href: "",
+            label: "Join Applications",
+          },
+          {
+            href: "",
+            label: "Withdraw an Application",
+          },
+        ],
+      },
+      {
+        title: "How to Complete a Paper Application for Dispute Resolutions",
+        description:
+          "Follow these tips and suggestions in completing your paper application for Dispute Resolution properly",
+      },
+      {
+        title: "Hearing process",
+        description:
+          "Find important information about the dispute resolution and the dispute resolution hearing.",
+      },
+      {
+        title: "Prepare for your Hearing",
+        description:
+          "Find important information about the dispute resolution and the dispute resolution hearing.",
+      },
+      {
+        title: "Fees & Fee Waivers",
+        description:
+          "See filing fees for different dispute resolution applications and see if you qualify for a fee waiver",
+      },
+    ],
+  },
+];
+
+export { sections };
