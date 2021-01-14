51839fee9f2aaa615993dfffe2f587ff8a07541c
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-01-13 16:15:40 -0800

    Add instance of RadioButtonContent to Online Dispute Resolution Application page

--- react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/Online-Dispute-Resolution-Application/data.js
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/Online-Dispute-Resolution-Application/data.js
@@ -76,33 +76,188 @@ const content = [
     ],
   },
   {
-    type: "wizard",
+    type: "radio-button-group",
+    id: "dispute-resolution",
     title: "I'm a...",
-    first: "intro",
-    steps: {
-      intro: {
-        options: [
+    children: [
+      {
+        id: "tenant",
+        label: "Tenant",
+        body: [
           {
-            label: "Tenant",
-            id: "tenant",
-            next_step: "",
-            content: [],
+            type: "h3",
+            children: "Common Reasons to apply for a dispute resolution",
           },
           {
-            label: "Landlord",
-            id: "landlord",
-            next_step: "",
-            content: [],
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children: "- Dispute a Notice to End Tenancy",
+              },
+            ],
           },
           {
-            label: "Respondent",
-            id: "respondent",
-            next_step: "",
-            content: [],
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "- Have a landlord make repairs to the rental unit or property",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "- Apply for compensation from a landlord for money owed or other tenancy-related issues",
+              },
+            ],
+          },
+          {
+            type: "br",
+          },
+          {
+            type: "h3",
+            children: "Direct Requests",
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "When a tenancy ends and the landlord receives the tenant’s forwarding address in writing, the landlord has fifteen days to either return the outstanding deposit(s) or make an application to retain part or all of the deposit(s).",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "If the landlord does nothing, tenants have an alternative option to get a Monetary Order. This is called a Direct Request.",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children: "- ",
+              },
+              {
+                type: "a-internal",
+                children: "Learn How to Apply for a Tenant’s Direct Request",
+                href: "/",
+              },
+            ],
           },
         ],
       },
-    },
+      {
+        id: "landlord",
+        label: "Landlord",
+        body: [
+          {
+            type: "h3",
+            children: "Common Reasons to apply for a dispute resolution",
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "- Apply for an Order of Possession because a tenant hasn’t moved out when they should",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "- Claim money from a tenant for unpaid rent or damages",
+              },
+            ],
+          },
+          {
+            type: "br",
+          },
+          {
+            type: "h3",
+            children: "Direct Requests",
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "When a tenant receives a 10 Day Notice to End Tenancy for Unpaid Rent or Utilities, they have five days to either pay the overdue amount or dispute it. If the tenant does nothing, landlords have an alternative option to get an Order of Possession. This is called a Landlord Direct Request.",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "A landlord can use a Direct Request to claim for an order of possession when a tenant hasn’t paid their rent or utilities and a monetary order for unpaid rent or utilities or payment of the filing fee.",
+              },
+            ],
+          },
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children: "- ",
+              },
+              {
+                type: "a-internal",
+                children: "Learn How to Apply for a Landlord’s Direct Request",
+                href: "/",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: "respondent",
+        label: "Respondent",
+        body: [
+          {
+            type: "p",
+            children: [
+              {
+                type: "text",
+                children:
+                  "As a respondent, you’d have received a Notice of Dispute Resolution Hearing Package with a Dispute Access Code. With that you can add your evidence online though the Dispute Access Site.",
+              },
+            ],
+          },
+          {
+            type: "button-link",
+            children: "Dispute Access Site",
+            external: "false",
+            href: "/",
+            primary: true,
+          },
+          {
+            type: "br",
+          },
+        ],
+      },
+    ],
   },
   {
     type: "back-forward-button-pair",
@@ -182,7 +337,7 @@ const content = [
         type: "button-link",
         children: "Contact the Branch",
         external: "false",
-        href: "",
+        href: "/",
         primary: true,
       },
     ],
