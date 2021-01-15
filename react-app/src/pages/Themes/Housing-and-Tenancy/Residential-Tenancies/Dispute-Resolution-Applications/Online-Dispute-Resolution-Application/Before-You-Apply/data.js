f55b35f2a43ebbed00ff6ec5bed12ac8b668d420
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-01-14 16:07:57 -0800

    Add Before You Apply page with first Accordion data

---
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/Online-Dispute-Resolution-Application/Before-You-Apply/data.js
@@ -0,0 +1,346 @@
+const content = [
+  {
+    type: "p",
+    className: "p--last-updated",
+    children: [
+      {
+        type: "text",
+        style: "normal",
+        children: "Last Updated: December 12, 2020",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "stepped-guide",
+    callToAction: {
+      label: "Apply Online",
+      href: "",
+    },
+    children: [
+      {
+        label: "Overview",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application",
+      },
+      {
+        label: "Before You Apply",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/before-you-apply",
+      },
+      {
+        label: "How to Pay",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/how-to-pay",
+      },
+      {
+        label: "How to Apply",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/how-to-apply",
+      },
+      {
+        label: "After You Apply",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/after-you-apply",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "h2",
+    id: "before-you-apply",
+    children: "Before You Apply",
+  },
+  {
+    type: "accordion",
+    id: "1-check-your-deadlines-and-calculations",
+    title: "1. Check Your Deadlines and Calculations",
+    children: [
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            children:
+              "Different types of disputes can have different application deadlines. Make sure you meet important deadlines and that your calculations are correct. Use these tools to help you determine important deadlines or calculations.",
+          },
+        ],
+      },
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            children:
+              "These calculators may be used for tenancies that fall under the Residential Tenancy Act. If your tenancy falls under the Manufactured Home Park Tenancy Act or if you are unsure which law governs your tenancy, please contact the ",
+          },
+          {
+            type: "a-internal",
+            href: "/",
+            children: "Residential Tenancy Branch",
+          },
+          {
+            type: "text",
+            children: ".",
+          },
+        ],
+      },
+      {
+        type: "h3",
+        children: "Calculators",
+      },
+      {
+        type: "navigation",
+        children: [
+          {
+            cards: [
+              {
+                title: "Disputing a Notice to End Tenancy",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children:
+                          "Calculate the date a tenant must file for dispute resolution to cancel a Notice to End Tenancy",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title: "Disputing a Notice to End Tenancy",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children: "Calculate interest on security deposits",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title: "When a tenant should receive their deposit(s)",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children:
+                          "Calculate the last date that a landlord (or landlord’s agent) must return the tenant’s security and/or pet damage deposit(s)",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title: "When and how much the rent can be increased",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children:
+                          "Calculate when a landlord can increase a tenant’s rent and the maximum allowable amount under the law",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title: "When a landlord can file for an order of possession",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children:
+                          "Calculate when a landlord can apply to the Residential Tenancy Branch for an order of possession of the rental property",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title:
+                  "When a landlord must return a deposit(s) or apply to keep it",
+                description: [
+                  {
+                    type: "p",
+                    children: [
+                      {
+                        type: "text",
+                        children:
+                          "Calculate the deadline to return a deposit(s) to a tenant or the deadline for which landlord must apply to the Residential Tenancy Branch to keep all or part of the deposit(s)",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            children:
+              "If it’s close to the deadline for applying, it’s best to submit your application and complete payment online or submit your application in person to a Service BC Office or the Burnaby Residential Tenancy Branch Office.",
+          },
+        ],
+      },
+      {
+        type: "callout",
+        children: [
+          {
+            type: "text",
+            children:
+              "Your application is not considered to be made until you have submitted your application and paid the filing fee or submitted documents for a fee waiver.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "accordion",
+    id: "2-collect-the-right-evidence",
+    title: "2. Collect the Right Evidence",
+    children: [],
+  },
+  {
+    type: "accordion",
+    id: "3-digitize-your-evidence",
+    title: "3. Digitize Your Evidence",
+    children: [],
+  },
+  {
+    type: "accordion",
+    id: "4-upload-evidence-in-the-right-format",
+    title: "4. Upload Evidence in the Right Format",
+    children: [],
+  },
+  {
+    type: "accordion",
+    id: "5-submit-your-evidence",
+    title: "5. Submit Your Evidence",
+    children: [],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "back-forward-button-pair",
+    args: {
+      backHref:
+        "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/",
+      backLabel: "Overview",
+      forwardHref:
+        "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/before-you-apply",
+      forwardLabel: "Before You Apply",
+    },
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "navigation",
+    children: [
+      {
+        title: "Related Services",
+        cards: [
+          {
+            title: "Complete a Paper Application",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children: "Learn how to complete a paper application",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Apply for a Tenant Direct Request",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Apply for a Landlord Direct Request",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "full-width-block",
+    children: [
+      {
+        type: "h2",
+        children: "Contact the Residential Tenancy Branch",
+        id: "contact-the-rtb",
+      },
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            children: "Contact us via phone, email, or in person",
+          },
+        ],
+      },
+      {
+        type: "button-link",
+        children: "Contact the Branch",
+        external: "false",
+        href: "/",
+        primary: true,
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+];
+
+export { content };
