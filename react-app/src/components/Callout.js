e3f8e41f7db248d55075f93b3b59e3b4b932f4dc
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-01-20 12:14:19 -0800

    Add className param to Callout component to support different colors

--- react-app/src/components/Callout.js
+++ react-app/src/components/Callout.js
@@ -4,14 +4,45 @@ import styled from "styled-components";
 import { textService } from "../_services/text.service";
 
 const StyledDiv = styled.div`
-  border-left: 6px solid #fcba19;
+  border-left: 6px solid;
   margin: 20px 0;
   padding: 0 0 0 18px;
+
+  &.error {
+    border-color: #fc1919;
+  }
+  &.info {
+    border-color: #003366;
+  }
+  &.success {
+    border-color: #2d4821;
+  }
+  &.warning {
+    border-color: #fcba19;
+  }
 `;
 
-function Callout({ children }) {
+function Callout({ children, className }) {
+  let style = "";
+
+  switch (className) {
+    case "error":
+      style = "error";
+      break;
+    case "info":
+      style = "info";
+      break;
+    case "success":
+      style = "success";
+      break;
+    case "warning":
+    default:
+      style = "warning";
+      break;
+  }
+
   return (
-    <StyledDiv>
+    <StyledDiv className={style}>
       {children.map((child, index) => {
         return textService.buildHtmlElement(child, index);
       })}
