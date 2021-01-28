9a8a8106931c2ead00bfc0ccab57940ea20a0889
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-01-28 12:49:33 -0800

    Add Under Construction page

---
+++ react-app/src/pages/Under-Construction/data.js
@@ -0,0 +1,13 @@
+const content = [
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children: "This page isn't finished yet.",
+      },
+    ],
+  },
+];
+
+export { content };
