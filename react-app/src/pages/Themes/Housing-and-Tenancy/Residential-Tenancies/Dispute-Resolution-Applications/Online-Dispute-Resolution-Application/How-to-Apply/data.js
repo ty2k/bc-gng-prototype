9c6a57ee5c05dd3420eea45af4ba00d7c9e765a1
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-03 14:45:04 -0800

    Add RTB ODRA How to Apply page

---
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/Online-Dispute-Resolution-Application/How-to-Apply/data.js
@@ -0,0 +1,307 @@
+const content = [
+  {
+    type: "p",
+    className: "p--last-updated",
+    children: [
+      {
+        type: "text",
+        style: "normal",
+        children: "Last Updated: December 12, 2020",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "stepped-guide",
+    callToAction: {
+      label: "Apply Online",
+      href: "/under-construction",
+    },
+    children: [
+      {
+        label: "Overview",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application",
+      },
+      {
+        label: "Before You Apply",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/before-you-apply",
+      },
+      {
+        label: "How to Pay",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/how-to-pay",
+      },
+      {
+        label: "How to Apply",
+        href:
+          "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/how-to-apply",
+      },
+      {
+        label: "After You Apply",
+        href: "/under-construction",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "on-this-page",
+    title: "On this Page",
+    children: [
+      {
+        id: "apply-online",
+        label: "Apply Online",
+      },
+      {
+        id: "complete-a-paper-application",
+        label: "Complete a Paper Application",
+      },
+      {
+        id: "related-services",
+        label: "Related Services",
+      },
+      {
+        id: "contact-the-rtb",
+        label: "Contact the Residential Tenancy Branch",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "h2",
+    id: "apply-online",
+    children: "Apply Online",
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children:
+          "Do you have everything ready to apply online? Click Apply Online.",
+      },
+    ],
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        style: "strong",
+        children: "IMPORTANT",
+      },
+      {
+        type: "text",
+        children:
+          ": before starting an application, please make sure your internet browser is up to date. ",
+      },
+    ],
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children: "- ",
+      },
+      {
+        type: "a-internal",
+        href: "/under-construction",
+        children: "View existing applications",
+      },
+    ],
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children:
+          "If you submitted an online application before September 23, 2017, you can contact the Residential Tenancy Branch for questions, changes or to receive a copy of your application.",
+      },
+    ],
+  },
+  {
+    type: "button-link",
+    children: "Apply Online",
+    external: false,
+    href: "/under-construction",
+    primary: true,
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "h2",
+    id: "complete-a-paper-application",
+    children: "Complete a Paper Application",
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children: "Not able to apply online? No problem.",
+      },
+    ],
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children:
+          "While applying online is the fastest, easiest way to apply, you may complete a paper application and submit it in person at any ",
+      },
+      {
+        type: "a-internal",
+        href: "/under-construction",
+        children: "Service BC Office",
+      },
+      {
+        type: "text",
+        children: " or the ",
+      },
+      {
+        type: "a-internal",
+        href: "/under-construction",
+        children: "Residential Tenancy Branch Office in Burnaby",
+      },
+      {
+        type: "text",
+        children: ".",
+      },
+    ],
+  },
+  {
+    type: "p",
+    children: [
+      {
+        type: "text",
+        children: "- Learn ",
+      },
+      {
+        type: "a-internal",
+        href: "/under-construction",
+        children: "how to complete a paper application",
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "back-forward-button-pair",
+    args: {
+      backHref:
+        "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/how-to-pay",
+      backLabel: "How to Pay",
+      forwardHref:
+        "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application/after-you-apply",
+      forwardLabel: "After You Apply",
+    },
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "h2",
+    id: "related-services",
+    children: "Related Services",
+  },
+  {
+    type: "navigation",
+    children: [
+      {
+        title: "",
+        cards: [
+          {
+            title: "Complete a Paper Application",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children: "Learn how to complete a paper application",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Apply for a Tenant Direct Request",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Apply for a Landlord Direct Request",
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+  {
+    type: "full-width-block",
+    children: [
+      {
+        type: "h2",
+        children: "Contact the Residential Tenancy Branch",
+        id: "contact-the-rtb",
+      },
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            children: "Contact us via phone, email, or in person",
+          },
+        ],
+      },
+      {
+        type: "button-link",
+        children: "Contact the Branch",
+        external: false,
+        href: "/under-construction",
+        primary: true,
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+];
+
+export { content };
