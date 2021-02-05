fdd4689b8892ce1f4d7dd213c9e72a2653b54827
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-05 15:54:18 -0800

    Add SeeMoreButton functionality to Homepage topics cards

--- react-app/src/pages/Home/index.js
+++ react-app/src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import MediaQuery from "react-responsive";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -87,6 +88,10 @@ const TopicsCard = styled.div`
   flex-direction: column;
   justify-content: space-between;
 
+  &.hidden {
+    display: none;
+  }
+
   div.card--icon-title {
     align-items: center;
     display: flex;
@@ -137,6 +142,12 @@ const TopicsCard = styled.div`
 `;
 
 function Home() {
+  const [mobileAllShown, setMobileAllShown] = useState(false);
+
+  function handleSeeMore() {
+    setMobileAllShown(true);
+  }
+
   return (
     <>
       {/* Full width block with personalization component */}
@@ -156,41 +167,100 @@ function Home() {
       </HighlightsGrid>
 
       {/* 16 Topic tiles */}
-      <TopicsGrid>
-        {topics?.map(({ icon, title, cardLink, links }, index) => {
-          return (
-            <TopicsCard key={`homepage-topic-${index}`}>
-              <div className="card--icon-title">
-                {icon && <Icon id={icon} />}
-                {title &&
-                  (cardLink ? (
-                    <Link to={cardLink.href}>{title && <h3>{title}</h3>}</Link>
-                  ) : (
-                    <h3>{title}</h3>
-                  ))}
-              </div>
-              {links?.length > 0 && (
-                <ul>
-                  {links.map((link, index) => {
-                    return (
-                      <li key={`li-${index}`}>
-                        {link.external ? (
-                          <>
-                            <a href={link.href}>{link.label}</a>
-                            <Icon id={"external-link-alt-solid.svg"} />
-                          </>
-                        ) : (
-                          <Link to={link.href}>{link.label}</Link>
-                        )}
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </TopicsCard>
-          );
-        })}
-      </TopicsGrid>
+
+      {/* Non-mobile, show all cards */}
+      <MediaQuery minWidth={576}>
+        <TopicsGrid>
+          {topics?.map(({ icon, title, cardLink, links }, index) => {
+            return (
+              <TopicsCard key={`homepage-topic-${index}`}>
+                <div className="card--icon-title">
+                  {icon && <Icon id={icon} />}
+                  {title &&
+                    (cardLink ? (
+                      <Link to={cardLink.href}>
+                        {title && <h3>{title}</h3>}
+                      </Link>
+                    ) : (
+                      <h3>{title}</h3>
+                    ))}
+                </div>
+                {links?.length > 0 && (
+                  <ul>
+                    {links.map((link, index) => {
+                      return (
+                        <li key={`li-${index}`}>
+                          {link.external ? (
+                            <>
+                              <a href={link.href}>{link.label}</a>
+                              <Icon id={"external-link-alt-solid.svg"} />
+                            </>
+                          ) : (
+                            <Link to={link.href}>{link.label}</Link>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </TopicsCard>
+            );
+          })}
+        </TopicsGrid>
+      </MediaQuery>
+
+      {/* Mobile, show max of six cards and a see more button initially */}
+      <MediaQuery maxWidth={575}>
+        <TopicsGrid>
+          {topics?.map(({ icon, title, cardLink, links }, index) => {
+            return (
+              <TopicsCard
+                key={`homepage-topic-${index}`}
+                className={!mobileAllShown && index >= 6 && "hidden"}
+              >
+                <div className="card--icon-title">
+                  {icon && <Icon id={icon} />}
+                  {title &&
+                    (cardLink ? (
+                      <Link to={cardLink.href}>
+                        {title && <h3>{title}</h3>}
+                      </Link>
+                    ) : (
+                      <h3>{title}</h3>
+                    ))}
+                </div>
+                {links?.length > 0 && (
+                  <ul>
+                    {links.map((link, index) => {
+                      return (
+                        <li key={`li-${index}`}>
+                          {link.external ? (
+                            <>
+                              <a href={link.href}>{link.label}</a>
+                              <Icon id={"external-link-alt-solid.svg"} />
+                            </>
+                          ) : (
+                            <Link to={link.href}>{link.label}</Link>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </TopicsCard>
+            );
+          })}
+          {!mobileAllShown && topics?.length > 6 && (
+            <SeeMoreButton
+              onClick={() => {
+                handleSeeMore();
+              }}
+            >
+              View More Topics
+            </SeeMoreButton>
+          )}
+        </TopicsGrid>
+      </MediaQuery>
 
       {/* Work in BC full width block */}
 
@@ -202,6 +272,36 @@ function Home() {
 
       {/* Footer */}
     </>
+  );
+}
+
+const StyledSeeMoreButton = styled.button`
+  background: none;
+  border: 0;
+  display: block;
+  margin: 0;
+  min-height: 44px;
+  padding: 0;
+  width: 100%;
+
+  p {
+    font-size: 18px;
+    font-weight: 700;
+    margin: 18px 0 8px 0;
+  }
+
+  svg {
+    max-height: 20px;
+    max-width: 50px;
+  }
+`;
+
+function SeeMoreButton({ children, onClick }) {
+  return (
+    <StyledSeeMoreButton onClick={onClick} type="button">
+      <p>{children}</p>
+      <Icon id={"chevron-down-gold.svg"} />
+    </StyledSeeMoreButton>
   );
 }
 
