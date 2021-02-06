0228137d59751f0980fe8286a41afebc7973151a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-05 16:30:23 -0800

    Add initial Personalization block to Homepage

--- react-app/src/pages/Home/index.js
+++ react-app/src/pages/Home/index.js
@@ -6,6 +6,53 @@ import styled from "styled-components";
 import Icon from "../../components/Icon";
 import { topics, highlightTopics } from "./data.js";
 
+const Personalization = styled.div`
+  background-color: #f2f2f2;
+  height: 351px;
+  left: 0;
+  position: absolute;
+  width: 100%;
+
+  div.div--interior {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin: 0 auto;
+
+    @media (min-width: 576px) {
+      width: 100%;
+    }
+    @media (min-width: 768px) {
+      width: 728px;
+    }
+    @media (min-width: 992px) {
+      width: 952px;
+    }
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2.h2--intro-text {
+    display: block;
+  }
+  input[type="text"].input--personalization {
+    background: none;
+    border: none;
+    border-bottom: 1px solid #313132;
+    font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
+    font-size: 18px;
+  }
+`;
+
+const Spacer = styled.div`
+  height: 351px;
+`;
+
 const HighlightsGrid = styled.div`
   column-gap: 24px;
   display: grid;
@@ -150,7 +197,31 @@ function Home() {
 
   return (
     <>
-      {/* Full width block with personalization component */}
+      {/* Full width block personalization block */}
+      <Personalization>
+        <div className={"div--interior"}>
+          <Column>
+            <h2 className={"h2--intro-text"}>I'm...</h2>
+            <input type="text" className={"input--personalization"} />
+          </Column>
+          <Column>
+            <p>Employment</p>
+          </Column>
+          <Column>
+            <p>Education</p>
+          </Column>
+          <Column>
+            <p>Home</p>
+          </Column>
+          <Column>
+            <p>Family</p>
+          </Column>
+          <Column>
+            <p>Right Arrow</p>
+          </Column>
+        </div>
+      </Personalization>
+      <Spacer />
 
       {/* 4 highlight tiles with larger icons */}
       <HighlightsGrid>
