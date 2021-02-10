430dd3760c2201e64e3051daeef8aa9b3d7f7079
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-10 11:09:35 -0800

    Add under construction links to RTB Dispute Resolution Applications page cards

--- react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/data.js
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/data.js
@@ -25,13 +25,25 @@ const sections = [
         title: "Apply for a Landlord Direct Request",
         description:
           "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Dispute Access Site",
         description: "Are you a respondent? Add your evidence online!",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "View or Modify an Existing Application",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
         links: [
           {
             href: "/under-construction",
@@ -55,21 +67,37 @@ const sections = [
         title: "How to Complete a Paper Application for Dispute Resolutions",
         description:
           "Follow these tips and suggestions in completing your paper application for Dispute Resolution properly",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Hearing process",
         description:
           "Find important information about the dispute resolution and the dispute resolution hearing.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Prepare for your Hearing",
         description:
           "Find important information about the dispute resolution and the dispute resolution hearing.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Fees & Fee Waivers",
         description:
           "See filing fees for different dispute resolution applications and see if you qualify for a fee waiver",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
     ],
   },
