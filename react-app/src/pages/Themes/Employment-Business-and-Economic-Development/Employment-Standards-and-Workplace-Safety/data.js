8ce75b15d40937e2729181a7cae2be7549781093
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-10 10:18:08 -0800

    Add under construction links to Employment Standards and Workplace Safety page cards

--- react-app/src/pages/Themes/Employment-Business-and-Economic-Development/Employment-Standards-and-Workplace-Safety/data.js
+++ react-app/src/pages/Themes/Employment-Business-and-Economic-Development/Employment-Standards-and-Workplace-Safety/data.js
@@ -13,44 +13,80 @@ const sections = [
       {
         title: "Personal Injury & Workplace Safety",
         description: "Get help with disagreements with WorkSafeBC decisions",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Employment Standards Act",
         description: "Read the full Employment Standards Act",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Employment Standards Tribunal",
         description:
           "Appeal decisions made with regards to the Employment Standards Act",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "WorkSafeBC",
         description:
           "Did you suffer from a work-related injury or disease? We can help with return-to-work rehabilitation, compensation, health care benefits and other services.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "The BC Labour Relations Board",
         description:
           "Mediates and adjudicates employment and labour relations matters related to unionized workplaces",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Workers’ Compensation Appeal Tribunal",
         description:
           "Appeal decisions made by WorkSafeBC on compensation, rehabilitation and occupational health and safety",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Employers’ Advisers Office",
         description: "Get help and advice regarding workplace safety",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "B.C. Human Rights Tribunal",
         description:
           "Read the Human Rights Code, file a human rights complaint, or search for a decision.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
       {
         title: "Contact the Employment Standards Branch",
         description:
           "Contact the Employment Standards Branch for via phone, text, email our visit the offices.",
+        cardLink: {
+          href: "/under-construction",
+          external: false,
+        },
       },
     ],
   },
