286fd0fee697eb97abcb73aa0b6d1cab8866235a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-18 15:20:39 -0800

    Add Navigation card content to Education and Training page

--- react-app/src/pages/Themes/Education-and-Training/data.js
+++ react-app/src/pages/Themes/Education-and-Training/data.js
@@ -1,3 +1,249 @@
-const content = [];
+const content = [
+  {
+    type: "br",
+  },
+  {
+    type: "navigation",
+    id: "education-and-training-navigation",
+    search: {
+      label: "Search Education and Training",
+    },
+    children: [
+      {
+        cards: [
+          {
+            title: "Blueprint Builder",
+            cardLink: {
+              href: "/under-construction",
+              external: true,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "The Blueprint Builder is your hub to access B.C. career tools and resources.",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Transcripts and Certificates",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Find out how to access your secondary school marks and credits.",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: "Early Learning",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Find resources for childhood educators and administrators, and supports for early development",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "StrongStart BC",
+              },
+              {
+                href: "/under-construction",
+                label: "Early Learning Framework",
+              },
+            ],
+          },
+          {
+            title: "Kindergarten to Grade 12",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Find a variety of educational tools designed to support learning.",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "Resources for teachers",
+              },
+              {
+                href: "/under-construction",
+                label: "COVID-19 safe schools",
+              },
+              {
+                href: "/under-construction",
+                label: "StudentTranscripts Service",
+              },
+            ],
+          },
+          {
+            title: "Post-secondary Education",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Learn about your options and supports for post-secondary education",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "Transcripts & Credentials",
+              },
+              {
+                href: "/under-construction",
+                label: "Find a Program or Institution",
+              },
+              {
+                href: "/under-construction",
+                label: "Pay for School",
+              },
+            ],
+          },
+          {
+            title: "Adult Education",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children:
+                      "Explore options for mature students who want to graduate high school or take courses to meet post-secondary program requirements.",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "British Columbia Adult Graduation Diploma Program",
+              },
+              {
+                href: "/under-construction",
+                label: "Learn English",
+              },
+              {
+                href: "/under-construction",
+                label: "Adult Special Education",
+              },
+            ],
+          },
+          {
+            title: "Ways to Learn",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children: "Find flexible learning options.",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "BC K-12 School and District Contact Information",
+              },
+              {
+                href: "/under-construction",
+                label: "Find a Post-secondary Institution",
+              },
+            ],
+          },
+          {
+            title: "Job and Career Training",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description: [
+              {
+                type: "p",
+                children: [
+                  {
+                    type: "text",
+                    children: "Find career resources, options and training.",
+                  },
+                ],
+              },
+            ],
+            links: [
+              {
+                href: "/under-construction",
+                label: "Training programs",
+              },
+              {
+                href: "/under-construction",
+                label: "Training Preparation",
+              },
+              {
+                href: "/under-construction",
+                label: "Apprenticeships & Industry Training",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "br",
+  },
+];
 
 export { content };
