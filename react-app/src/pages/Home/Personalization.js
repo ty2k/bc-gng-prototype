e66e5a73b8f70f6553dd3cbc80b66719e0b78881
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-02-22 15:28:23 -0800

    Refactor Homepage Personalization block to remove media queries

--- react-app/src/pages/Home/Personalization.js
+++ react-app/src/pages/Home/Personalization.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Highlighter from "react-highlight-words";
-import MediaQuery from "react-responsive";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -9,13 +8,13 @@ const PersonalizationBlock = styled.div`
   min-height: 272px;
   width: 100%;
 
-  div.div--interior {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    margin: 0 auto;
-    padding: 20px 0;
+  div.div--container {
+    display: inline-block;
+    vertical-align: top;
 
+    @media (max-width: 575px) {
+      width: 100%;
+    }
     @media (min-width: 576px) {
       width: 100%;
     }
@@ -23,35 +22,54 @@ const PersonalizationBlock = styled.div`
       width: 728px;
     }
     @media (min-width: 992px) {
-      width: 952px;
+      width: 752px;
+    }
+
+    div.div--flex {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      margin: 0 auto;
+      padding: 20px 0;
+
+      @media (max-width: 767px) {
+        overflow-x: auto;
+        width: 100%;
+
+        /* Hide scrollbar */
+        -ms-overflow-style: none; /* Internet Explorer 10+ */
+        scrollbar-width: none; /* Firefox */
+        &::-webkit-scrollbar {
+          display: none; /* Chrome, Safari */
+        }
+      }
     }
   }
+`;
 
-  div.div--interior-mobile {
-    justify-content: flex-start;
-    overflow-x: auto;
+const SearchBlock = styled.div`
+  display: inline-block;
+  text-align: center;
+  vertical-align: center;
+  width: 200px;
+
+  @media (max-width: 991px) {
+    display: block;
     width: 100%;
-
-    /* Hide scrollbar */
-    -ms-overflow-style: none; /* Internet Explorer 10+ */
-    scrollbar-width: none; /* Firefox */
-    &::-webkit-scrollbar {
-      display: none; /* Chrome, Safari */
-    }
-
-    @media (min-width: 576px) {
-      width: 100%;
-    }
-    @media (min-width: 768px) {
-      width: 728px;
-    }
-    @media (min-width: 992px) {
-      width: 952px;
-    }
   }
 
-  div.personalization-search-horizontal {
-    text-align: center;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+      display: block;
+
+      @media (min-width: 992px) {
+        margin-top: 40px;
+      }
+    }
 
     label {
       display: none;
@@ -61,45 +79,24 @@ const PersonalizationBlock = styled.div`
       background: none;
       border: none;
       border-bottom: 1px solid #313132;
+      border-radius: 0;
       font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
       font-size: 18px;
       padding: 0;
-      width: 200px;
-    }
-  }
+      width: 150px;
 
-  .text--highlighted {
-    background-color: #fcba19;
+      @media (max-width: 991px) {
+        width: 200px;
+      }
+    }
   }
 `;
 
 const Column = styled.div`
   display: block;
   text-align: left;
-  max-width: 200px;
+  max-width: 190px;
   min-width: 180px;
-
-  &.personalization-search-column {
-    text-align: center;
-  }
-
-  h2.h2--intro-text {
-    display: block;
-  }
-
-  label {
-    display: none;
-  }
-
-  input[type="text"].input--personalization {
-    background: none;
-    border: none;
-    border-bottom: 1px solid #313132;
-    font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
-    font-size: 18px;
-    padding: 0;
-    width: 150px;
-  }
 
   a {
     color: #313132;
@@ -141,8 +138,14 @@ const Column = styled.div`
     height: 100px;
     margin-top: 16px;
   }
+
+  .text--highlighted {
+    background-color: #fcba19;
+  }
 `;
 
+// If a full width block with position: absolute is used for full screen
+// width, a Spacer div should be used to push the rest of the content down
 const Spacer = styled.div`
   height: 420px;
 `;
@@ -163,122 +166,71 @@ function Personalization({ personalization, parentCallback, searchTerm }) {
   });
 
   return (
-    <>
-      <PersonalizationBlock id={id}>
-        {/* Desktop, show all columns with search column on left */}
-        <MediaQuery minWidth={576}>
-          <div className={"div--interior"}>
-            <Column className={"personalization-search-column"}>
-              {intro && <h2 className={"h2--intro-text"}>{intro}</h2>}
-              <label for="personalization-input">looking for</label>
-              <input
-                type="text"
-                autoComplete="off"
-                className="input--personalization"
-                id="personalization-input"
-                name="personalization-input"
-                onChange={(e) => {
-                  parentCallback(e.target.value);
-                }}
-              />
-            </Column>
+    <PersonalizationBlock id={id}>
+      {/* Desktop: SearchBlock appears as a left column
+      Mobile: SearchBlock appears as a full width row above the columns */}
+      <SearchBlock>
+        <div>
+          {intro && <h2>{intro}</h2>}
+          <label for="personalization-input">looking for</label>
+          <input
+            type="text"
+            autoComplete="off"
+            id="personalization-input"
+            name="personalization-input"
+            onChange={(e) => {
+              parentCallback(e.target.value);
+            }}
+          />
+        </div>
+      </SearchBlock>
 
-            {/* Show only the verticals that have been filtered */}
-            {filteredVerticals?.length > 0 &&
-              filteredVerticals.map(({ id, href, title, children }, index) => {
-                return (
-                  <Column key={`personalization-column-${id ? id : index}`}>
-                    {title && href && (
-                      <Link to={href}>
-                        <h3>{title}</h3>
-                      </Link>
-                    )}
+      <div className={"div--container"}>
+        <div className={"div--flex"}>
+          {/* Show only the verticals that have been filtered */}
+          {filteredVerticals?.length > 0 &&
+            filteredVerticals.map(({ id, href, title, children }, index) => {
+              return (
+                <Column key={`personalization-column-${id ? id : index}`}>
+                  {title && href && (
+                    <Link to={href}>
+                      <h3>{title}</h3>
+                    </Link>
+                  )}
 
-                    {/* Display all child links for a vertical and
-                    highlight those that match searchTerm */}
-                    {children?.length > 0 && (
-                      <ul>
-                        {children.map(
-                          ({ href: childHref, label }, childIndex) => {
-                            return (
-                              <li key={`column-${id}-li-${childIndex}`}>
-                                <Link to={childHref}>
-                                  <Highlighter
-                                    highlightClassName="text--highlighted"
-                                    searchWords={[searchTerm]}
-                                    autoEscape={true}
-                                    textToHighlight={label}
-                                  />
-                                </Link>
-                              </li>
-                            );
-                          }
-                        )}
-                      </ul>
-                    )}
-                  </Column>
-                );
-              })}
-            {/* <Column>
-              <Icon id={"homepage-right-arrow.svg"} />
-            </Column> */}
-          </div>
-        </MediaQuery>
+                  {/* Display all child links for a vertical and
+                  highlight those that match searchTerm */}
+                  {children?.length > 0 && (
+                    <ul>
+                      {children.map(
+                        ({ href: childHref, label }, childIndex) => {
+                          return (
+                            <li key={`column-${id}-li-${childIndex}`}>
+                              <Link to={childHref}>
+                                <Highlighter
+                                  highlightClassName="text--highlighted"
+                                  searchWords={[searchTerm]}
+                                  autoEscape={true}
+                                  textToHighlight={label}
+                                />
+                              </Link>
+                            </li>
+                          );
+                        }
+                      )}
+                    </ul>
+                  )}
+                </Column>
+              );
+            })}
 
-        {/* Mobile, show search bar on top and columns with horizontal scroll below */}
-        <MediaQuery maxWidth={575}>
-          <div className={"personalization-search-horizontal"}>
-            {intro && <h2 className={"h2--intro-text"}>{intro}</h2>}
-            <label for="personalization-input">looking for</label>
-            <input
-              type="text"
-              autoComplete="off"
-              className="input--personalization"
-              id="personalization-input"
-              name="personalization-input"
-              onChange={(e) => {
-                parentCallback(e.target.value);
-              }}
-            />
-          </div>
-          <div className={"div--interior div--interior-mobile"}>
-            {filteredVerticals?.length > 0 &&
-              filteredVerticals.map(({ id, href, title, children }, index) => {
-                return (
-                  <Column key={`personalization-column-${id ? id : index}`}>
-                    {title && href && (
-                      <Link to={href}>
-                        <h3>{title}</h3>
-                      </Link>
-                    )}
-                    {children?.length > 0 && (
-                      <ul>
-                        {children.map(
-                          ({ href: childHref, label }, childIndex) => {
-                            return (
-                              <li key={`column-${id}-li-${childIndex}`}>
-                                <Link to={childHref}>
-                                  <Highlighter
-                                    highlightClassName="text--highlighted"
-                                    searchWords={[searchTerm]}
-                                    autoEscape={true}
-                                    textToHighlight={label}
-                                  />
-                                </Link>
-                              </li>
-                            );
-                          }
-                        )}
-                      </ul>
-                    )}
-                  </Column>
-                );
-              })}
-          </div>
-        </MediaQuery>
-      </PersonalizationBlock>
-      {/* <Spacer /> */}
-    </>
+          {/* Right arrow to display more columns */}
+          {/* <Column>
+            <Icon id={"homepage-right-arrow.svg"} />
+          </Column> */}
+        </div>
+      </div>
+    </PersonalizationBlock>
   );
 }
 
