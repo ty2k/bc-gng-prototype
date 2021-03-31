104e4c72e81ddd5328e4b6af6c194d449ccfa7f0
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-03-31 15:10:02 -0700

    GNG-398: Refactor Dispute Resolution Applications to use Navigation component in Content

--- react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/data.js
+++ react-app/src/pages/Themes/Housing-and-Tenancy/Residential-Tenancies/Dispute-Resolution-Applications/data.js
@@ -1,106 +1,111 @@
-const sections = [
+const content = [
   {
-    title: "",
-    cards: [
+    type: "navigation",
+    children: [
       {
-        title: "Apply for a Dispute Resolution Application",
-        description: "Resolve disputes related to a tenancy.",
-        cardLink: {
-          href:
-            "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application",
-          external: false,
-        },
-      },
-      {
-        title: "Apply for a Tenant Direct Request",
-        description:
-          "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
-        cardLink: {
-          href:
-            "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/tenant-direct-request",
-          external: false,
-        },
-      },
-      {
-        title: "Apply for a Landlord Direct Request",
-        description:
-          "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-      },
-      {
-        title: "Dispute Access Site",
-        description: "Are you a respondent? Add your evidence online!",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-      },
-      {
-        title: "View or Modify an Existing Application",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
+        cards: [
           {
-            href: "/under-construction",
-            label: "Amend or Update an Application",
+            title: "Apply for a Dispute Resolution Application",
+            description: "Resolve disputes related to a tenancy.",
+            cardLink: {
+              href:
+                "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/online-dispute-resolution-application",
+              external: false,
+            },
           },
           {
-            href: "/under-construction",
-            label: "Make a Cross Application",
+            title: "Apply for a Tenant Direct Request",
+            description:
+              "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+            cardLink: {
+              href:
+                "/themes/housing-and-tenancy/residential-tenancies/dispute-resolution-applications/tenant-direct-request",
+              external: false,
+            },
           },
           {
-            href: "/under-construction",
-            label: "Join Applications",
+            title: "Apply for a Landlord Direct Request",
+            description:
+              "Apply for a Monetary Order for return of a security deposit and/or a pet damage deposit.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
           },
           {
-            href: "/under-construction",
-            label: "Withdraw an Application",
+            title: "Dispute Access Site",
+            description: "Are you a respondent? Add your evidence online!",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+          },
+          {
+            title: "View or Modify an Existing Application",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Amend or Update an Application",
+              },
+              {
+                href: "/under-construction",
+                label: "Make a Cross Application",
+              },
+              {
+                href: "/under-construction",
+                label: "Join Applications",
+              },
+              {
+                href: "/under-construction",
+                label: "Withdraw an Application",
+              },
+            ],
+          },
+          {
+            title:
+              "How to Complete a Paper Application for Dispute Resolutions",
+            description:
+              "Follow these tips and suggestions in completing your paper application for Dispute Resolution properly",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+          },
+          {
+            title: "Hearing process",
+            description:
+              "Find important information about the dispute resolution and the dispute resolution hearing.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+          },
+          {
+            title: "Prepare for your Hearing",
+            description:
+              "Find important information about the dispute resolution and the dispute resolution hearing.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+          },
+          {
+            title: "Fees & Fee Waivers",
+            description:
+              "See filing fees for different dispute resolution applications and see if you qualify for a fee waiver",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
           },
         ],
-      },
-      {
-        title: "How to Complete a Paper Application for Dispute Resolutions",
-        description:
-          "Follow these tips and suggestions in completing your paper application for Dispute Resolution properly",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-      },
-      {
-        title: "Hearing process",
-        description:
-          "Find important information about the dispute resolution and the dispute resolution hearing.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-      },
-      {
-        title: "Prepare for your Hearing",
-        description:
-          "Find important information about the dispute resolution and the dispute resolution hearing.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-      },
-      {
-        title: "Fees & Fee Waivers",
-        description:
-          "See filing fees for different dispute resolution applications and see if you qualify for a fee waiver",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
       },
     ],
   },
 ];
 
-export { sections };
+export { content };
