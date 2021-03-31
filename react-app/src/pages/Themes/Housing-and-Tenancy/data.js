8f54613793447d7b8e01fd237a69b6a9cc83cfd1
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-03-31 14:53:36 -0700

    GNG-398: Refactor Housing and Tenancy page to use Navigation component inside Content

--- react-app/src/pages/Themes/Housing-and-Tenancy/data.js
+++ react-app/src/pages/Themes/Housing-and-Tenancy/data.js
@@ -1,161 +1,169 @@
-const sections = [
+const content = [
   {
-    title: "",
-    cards: [
+    type: "navigation",
+    search: {
+      label: "Search Housing & Tenancy",
+    },
+    children: [
       {
-        title: "Residential Tenancies",
-        cardLink: {
-          href: "/themes/housing-and-tenancy/residential-tenancies",
-          external: false,
-        },
-        description: "Info and services for tenants and landlords.",
-        links: [
+        cards: [
           {
-            href: "/under-construction",
-            label: "Tips for landlords & renters",
+            title: "Residential Tenancies",
+            cardLink: {
+              href: "/themes/housing-and-tenancy/residential-tenancies",
+              external: false,
+            },
+            description: "Info and services for tenants and landlords.",
+            links: [
+              {
+                href: "/under-construction",
+                label: "Tips for landlords & renters",
+              },
+              {
+                href: "/themes/housing-and-tenancy/residential-tenancies/forms",
+                label: "Tenancy Agreements",
+              },
+            ],
           },
           {
-            href: "/themes/housing-and-tenancy/residential-tenancies/forms",
-            label: "Tenancy Agreements",
-          },
-        ],
-      },
-      {
-        title: "Strata Housing",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        description:
-          "Find out information about strata housing, the role of a strata council, budgets and bylaws.",
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-        ],
-      },
-      {
-        title: "Owning a Home",
-        description: "Find all information you need about owning a home.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Strata Housing",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            description:
+              "Find out information about strata housing, the role of a strata council, budgets and bylaws.",
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-        ],
-      },
-      {
-        title: "Seniors Housing",
-        description: "Find housing options for seniors and older adults in BC.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Owning a Home",
+            description: "Find all information you need about owning a home.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-        ],
-      },
-      {
-        title: "Affordable Housing",
-        description:
-          "Learn more about affordable housing, social housing, the role of government and BC Housing.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-        ],
-      },
-      {
-        title: "Local Governments & Housing",
-        description:
-          "Find affordable housing tools and resources available to local governments.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Seniors Housing",
+            description:
+              "Find housing options for seniors and older adults in BC.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-        ],
-      },
-      {
-        title: "Building or Renovating",
-        description:
-          "Find resources you may need when building or renovating your home.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
-          },
-          {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Affordable Housing",
+            description:
+              "Learn more about affordable housing, social housing, the role of government and BC Housing.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Local Governments & Housing",
+            description:
+              "Find affordable housing tools and resources available to local governments.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
-        ],
-      },
-      {
-        title: "Real Estate in B.C.",
-        description:
-          "Learn about Real Estate in B.C.: buying and selling property, real estate consultations, and regulations.",
-        cardLink: {
-          href: "/under-construction",
-          external: false,
-        },
-        links: [
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Building or Renovating",
+            description:
+              "Find resources you may need when building or renovating your home.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
           {
-            href: "/under-construction",
-            label: "Quick links to popular pages",
+            title: "Real Estate in B.C.",
+            description:
+              "Learn about Real Estate in B.C.: buying and selling property, real estate consultations, and regulations.",
+            cardLink: {
+              href: "/under-construction",
+              external: false,
+            },
+            links: [
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+              {
+                href: "/under-construction",
+                label: "Quick links to popular pages",
+              },
+            ],
           },
         ],
       },
@@ -163,4 +171,4 @@ const sections = [
   },
 ];
 
-export { sections };
+export { content };
--- react-app/src/pages/Themes/Housing-and-Tenancy/index.js
+++ react-app/src/pages/Themes/Housing-and-Tenancy/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-import Navigation from "../../../components/Navigation";
-import { sections } from "./data";
+import Content from "../../../components/Content";
+import { content } from "./data";
 
 function HousingAndTenancy() {
-  return (
-    <Navigation
-      search={{ label: "Search Housing & Tenancy" }}
-      sections={sections}
-    />
-  );
+  return <Content content={content} />;
 }
 
 export default HousingAndTenancy;
