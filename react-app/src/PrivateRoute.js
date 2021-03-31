0626d84b4f61bc44fd161e1559bcacf6ef9b576d
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-03-30 17:54:16 -0700

    GNG-398: Use react-helmet in PrivateRoute and Content components to overwrite static index.html metadata

--- react-app/src/PrivateRoute.js
+++ react-app/src/PrivateRoute.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Route, Redirect } from "react-router-dom";
 
 import Page from "./components/Page";
@@ -7,12 +8,21 @@ function PrivateRoute({
   title,
   breadcrumbs,
   content,
+  metadata,
   parentHref,
   parentTitle,
   ...rest
 }) {
   return localStorage.getItem("user") ? (
     <Route {...rest}>
+      {/* By defining the Helmet defaults here, we can keep the BC branding out
+      of the public Login route. In the future, this can live in App.js */}
+      <Helmet
+        defaultTitle="Province of British Columbia"
+        titleTemplate="%s - Province of British Columbia"
+      >
+        <meta name="description" content="Prototype BC Gov Next Gen site" />
+      </Helmet>
       <Page
         title={title}
         breadcrumbs={breadcrumbs}
