06c560b318e670a07ed40ce8e0ab3b99879ba814
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-04-21 19:45:49 -0700

    GNG-418: Add search functionality to Search page

--- react-app/src/pages/Search/index.js
+++ react-app/src/pages/Search/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+import { useHistory, useLocation } from "react-router-dom";
 import styled from "styled-components";
+import xml2js from "xml2js";
 
+import LoadSpinner from "../../components/LoadSpinner";
 import SearchBar from "../../components/SearchBar";
 
 const StyledSearchResults = styled.main`
@@ -19,15 +23,107 @@ function useQuery() {
 }
 
 function Search() {
-  // Look for query parameter in the form `?q=example`
-  const query = useQuery().get("q");
+  const [state, setState] = useState({
+    query: useQuery().get("q"), // Query parameter is in the form `?q=example`
+    isLoading: useQuery().get("q") ? true : false,
+    results: {},
+    newQuery: useQuery().get("q"),
+  });
+
+  let history = useHistory();
+
+  // Callback provided to SearchBar to update state with text field input
+  function updateNewQuery(input) {
+    setState({
+      ...state,
+      newQuery: input,
+    });
+  }
+
+  // Callback provided to SearchBar to conduct a new search
+  function submitNewQuery(event) {
+    event.preventDefault();
+    history.push(`/search?q=${state.newQuery}`);
+    setState({
+      query: state?.newQuery,
+      isLoading: state?.newQuery ? true : false,
+      results: {},
+      newQuery: state?.newQuery,
+    });
+  }
+
+  useEffect(() => {
+    // GET request to the search API using the query
+    axios.get(`/api/search?q=${state?.query}`).then((res) => {
+      // Parse XML response into JSON object
+      let parser = new xml2js.Parser();
+      parser.parseString(res?.data, (err, results) => {
+        if (err) {
+          setState({
+            ...state,
+            isLoading: false,
+          });
+        } else {
+          setState({
+            ...state,
+            isLoading: false,
+            results: results,
+          });
+        }
+      });
+    });
+  }, [state.query]);
 
   return (
     <StyledSearchResults>
-      <h1>
-        Search results for <strong>"{query}"</strong>
-      </h1>
-      <SearchBar initialInput={query} />
+      {/* Page metadata */}
+      <Helmet>
+        <title>
+          {state?.query?.length > 0
+            ? `Search results for "${state?.query}"`
+            : "Search"}
+        </title>
+        <meta
+          name={"description"}
+          content={
+            state?.query?.length > 0
+              ? `Search results for "${state?.query}"`
+              : `Search gov.bc.ca`
+          }
+        />
+      </Helmet>
+
+      {state?.query?.length > 0 ? (
+        <h1>
+          Search results for <strong>{`"${state.query}"`}</strong>
+        </h1>
+      ) : (
+        <h1>Search</h1>
+      )}
+      <SearchBar
+        initialInput={state?.query}
+        onButtonClick={(event) => {
+          submitNewQuery(event);
+        }}
+        parentCallback={updateNewQuery}
+      />
+
+      {/* Show a load spinner until the API request is completed */}
+      {state?.isLoading && <LoadSpinner />}
+
+      {/* Show a list of results or a message explaining there are no results */}
+      {state?.results &&
+      Object.keys(state?.results).length > 0 &&
+      state?.results?.GSP?.RES ? (
+        state?.results?.GSP?.RES[0]?.R?.map((result) => {
+          return <p>{result.T}</p>;
+        })
+      ) : (
+        <p>
+          Your search - <strong>{state.query}</strong> - did not match any
+          documents.
+        </p>
+      )}
     </StyledSearchResults>
   );
 }
